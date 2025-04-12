8796510e1f0948b1cf24248e7102470dc80a5c5d
c6 <31777460+c6-dev@users.noreply.github.com>
2025-04-13 00:17:01 +0400

    add door_helper

---
+++ door_helper/dllmain.cpp
@@ -0,0 +1,44 @@
+// credits to davi aka funkkiy for the original implementation with SafetyHook
+// TODO needs work - still seems to get stuck on the beach house door
+
+#define WIN32_LEAN_AND_MEAN
+#include "..\common\defs.h"
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+	return TRUE;
+}
+
+UInt32 vampire_base = 0;
+
+struct CGlobalVarsBase {
+	float realtime;
+	int framecount;
+	float absoluteframetime;
+	float curtime;
+	float frametime;
+};
+CGlobalVarsBase* gpGlobals = nullptr;
+
+void __fastcall AngularMoveHook(void* a1, void* edx, void* a2, float flSpeed)
+{
+	if (*(UInt32*)a1 == vampire_base + 0x454B94) { // CRotDoor only
+		if (gpGlobals == nullptr)
+		{
+			gpGlobals = *(CGlobalVarsBase**)(vampire_base + 0x70B228);
+		}
+		flSpeed *= (1.0f / gpGlobals->frametime) / 60.0f;
+	}
+	ThisCall(vampire_base + 0x1C17E0, a1, a2, flSpeed);
+}
+
+extern "C" __declspec(dllexport) void loaded_vampire()
+{
+	HMODULE vampire = GetModuleHandleA("vampire.dll");
+	if (vampire != NULL)
+	{
+		vampire_base = (UInt32)vampire;
+		SafeWriteBuf(vampire_base + 0xF3A66, (void*)"\xB9\x02\x00\x00\x00\x90\x90", 7);
+		WriteRelJump(vampire_base + 0xDAB2, (UInt32)AngularMoveHook);
+	}
+}
