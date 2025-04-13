cc6fca9f56ff65ac0400c02bea4875c9c3508fd7
c6 <31777460+c6-dev@users.noreply.github.com>
2025-04-13 16:40:09 +0400

    add more call templates

--- vtmb-tweaks/calls.h
+++ vtmb-tweaks/calls.h
@@ -10,3 +10,13 @@ __forceinline T_Ret ThisCall(UInt32 _addr, void* _this, Args ...args) {
 	} u = { _addr };
 	return ((T*)_this->*u.func)(std::forward<Args>(args)...);
 }
+
+template <typename T_Ret = void, typename ...Args>
+__forceinline T_Ret StdCall(UInt32 _addr, Args ...args) {
+	return ((T_Ret(__stdcall*)(Args...))_addr)(std::forward<Args>(args)...);
+}
+
+template <typename T_Ret = void, typename ...Args>
+__forceinline T_Ret CdeclCall(UInt32 _addr, Args ...args) {
+	return ((T_Ret(__cdecl*)(Args...))_addr)(std::forward<Args>(args)...);
+}
